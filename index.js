d236ca7bb50a6cbb10ea4eea2169f5e36ab0c47d
JoaoffPinho <joao.f.pinho@gmail.com>
2023-04-11 12:51:45 +0100

    test

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const auth = function(req, res, next) {
 }
 
 app.use(express.json());
-app.use(auth);
+// app.use(auth);
 app.use('/workers', workers);
 app.use('/projects', projects);
 app.use('/tasks', tasks);
