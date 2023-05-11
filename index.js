2afa154837f9cf62c32b9b2b83dc8a7edaf15ff8
JoaoffPinho <joao.f.pinho@gmail.com>
2023-05-11 11:31:06 +0100

    auth back

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const auth = function(req, res, next) {
 }
 
 app.use(express.json());
-// app.use(auth);
+app.use(auth);
 app.use(cors());
 app.use('/workers', workers);
 app.use('/projects', projects);
